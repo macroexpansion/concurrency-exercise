4fc05dd642fa311c026349f1c7316e526426b454
macroexpansion <trasuadev@gmail.com>
2024-03-27 19:33:37 +0700

    feat: finish main task

--- src/solution.rs
+++ src/solution.rs
@@ -1,5 +1,10 @@
-use crate::statement::*;
 use async_trait::async_trait;
+use tokio::{
+    sync::mpsc,
+    time::{interval, Duration},
+};
+
+use crate::statement::*;
 
 #[async_trait]
 pub trait Solution {
@@ -11,6 +16,40 @@ pub struct Solution0;
 #[async_trait]
 impl Solution for Solution0 {
     async fn solve(repositories: Vec<ServerName>) -> Option<Binary> {
-        todo!()
+        let repo_len = repositories.len();
+
+        let (ok_tx, mut ok_rx) = mpsc::channel::<Binary>(1);
+        let (err_tx, mut err_rx) = mpsc::channel::<()>(repositories.len());
+        let mut interval = interval(Duration::from_millis(100));
+
+        for repo in repositories {
+            let ok_tx = ok_tx.clone();
+            let err_tx = err_tx.clone();
+            let _ = tokio::spawn(async move {
+                let resp = download(repo).await.ok();
+
+                if resp.is_some() {
+                    let _ = ok_tx.send(resp.unwrap()).await;
+                } else {
+                    let _ = err_tx.send(()).await;
+                }
+            });
+        }
+
+        let mut err_count = 0;
+        loop {
+            tokio::select! {
+                _ = interval.tick() => println!("Another 100ms"),
+                Some(bin) = ok_rx.recv() => {
+                    return Some(bin);
+                },
+                Some(_) = err_rx.recv() => {
+                    err_count += 1;
+                    if err_count == repo_len {
+                        return None;
+                    }
+                }
+            }
+        }
     }
 }
--- src/statement.rs
+++ src/statement.rs
@@ -19,13 +19,17 @@ pub struct Binary {
 pub struct ServerName(pub String);
 
 pub async fn download(server_name: ServerName) -> Result<Binary, ServerError> {
+    println!("downloading {server_name:?}");
+
     let mut interval = time::interval(time::Duration::from_millis(100));
+
     for _i in 0..5 {
         interval.tick().await;
         if rand::random::<f32>() < 0.1 {
             return Err(ServerError::Disconnected(server_name.into()));
         }
     }
+
     Ok(Binary {
         from: server_name.into(),
     })
